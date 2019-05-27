de55a9b022924468ad033d4e6c161ac6b7a7344c
raksm <raksm@deloitte.com>
2019-05-27 08:02:49 +0530

    changes 7

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -47,7 +47,7 @@ public class EmployeeController {
  * @param emp the emp
  * @return the employee
  */
-@PostMapping("/addemployee")
+@PostMapping("/addEmployee")
 	public Employee addEmployee(Employee emp) {
 		
 		
